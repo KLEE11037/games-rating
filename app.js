b24ae21e132084abf5863f5983dbbfdce7357c67
KLEE11037 <zakiraza2009@yandex.ru>
2024-04-21 20:22:17 +0300

    added files for m4 hw1

---
+++ app.js
@@ -0,0 +1,25 @@
+const {
+  voteRouteController,
+  gameRouteController,
+  defaultRouteController,
+  mainRouteController,
+} = require("./controllers");
+const path = require("path");
+const http = require("http");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  switch (url) {
+    case "/":
+      mainRouteController(res, "/index.html", ".html");
+      break;
+    case "/game":
+      gameRouteController(res);
+      break;
+    case "/vote":
+      voteRouteController(req, res);
+      break;
+    default:
+      defaultRouteController(res, url);
+  }
+});
+server.listen(3005);
---
+++ appModules/http-utils/index.js
@@ -0,0 +1,9 @@
+const parseBody = require("./parse-body");
+const staticFile = require("./static-file");
+const mimeTypes = require("./mime-types")
+
+module.exports = {
+  parseBody,
+  staticFile,
+  mimeTypes
+};
